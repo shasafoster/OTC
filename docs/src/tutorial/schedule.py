6e6abf4635b50026e057c752f248deec4514134e
Shasa Foster <23664683+shasafoster@users.noreply.github.com>
2020-01-11 12:54:12 +1300

    Create schedule.py

---
+++ docs/src/tutorial/schedule.py
@@ -0,0 +1,8 @@
+## Derivative contract schedules
+
+# Over the counter (OTC) derivative contracts are highly customisable 
+# and can have payment schedules depending on a large number of parameters
+
+# 
+
+# * Payment holidays 
